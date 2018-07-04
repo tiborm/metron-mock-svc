0b9e493226673ba6d69b88d2338e99e199253645
tiborm <tibor.meller@gmail.com>
2018-07-04 14:32:04 +0200

    responding with the full pdml, refinement comming

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  
+const express = require('express');
+const router = express.Router();
+
+const pdml = fs.readFileSync('./mock-data/pdml.json', 'utf-8');
+
+router.get('/submit', function(req, res, next) {
+  res.status(200).send(pdml);
+});
+
+router.get('/status', function(req, res, next) {
+  res.status(200).send("");
+});
+
+
+router.get('/resultJson', function(req, res, next) {
+  res.status(200).send("");
 });
 
 module.exports = router;
