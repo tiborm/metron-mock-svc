682f8de82d99bc028766449a195e4b42cc232d35
tiborm <tibor.meller@gmail.com>
2018-07-12 10:34:34 +0200

    faking download endpoint with a dummy response pcap file

--- routes/pcap.js
+++ routes/pcap.js
@@ -1,22 +1,32 @@
 const fs = require('fs');
+const uuid = require('uuid/v1');
 
+const express = require('express');
 const pcapRouter = require('express').Router();
 
 const pdml = fs.readFileSync('./mock-data/pdml.json', 'utf-8');
 
 
 pcapRouter.post('/submit', function(req, res, next) {
-  res.status(200).send();
+  res.status(200).send(
+    JSON.stringify({
+      id: uuid()
+    })
+  );
 });
 
-pcapRouter.get('/status', function(req, res, next) {
+pcapRouter.get('/status', function (req, res, next) {
   // res.status(200).send("COMPLETE");
   res.status(200).send("Finished");
 });
 
 
-pcapRouter.get('/resultJson', function(req, res, next) {
+pcapRouter.get('/resultJson', function (req, res, next) {
   res.status(200).send(pdml);
+});
+
+pcapRouter.get('/download', function (req, res, next) {
+  res.sendFile(process.cwd() + '/mock-data/dummy.pcap');
 });
 
 module.exports = pcapRouter;
