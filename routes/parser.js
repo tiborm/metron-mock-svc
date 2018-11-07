5c2f36f04937a870654935c8e68c672d63764efc
tiborm <tibor.meller@gmail.com>
2018-11-07 14:55:26 +0100

    adding parser group endpoint and group data

--- routes/parser.js
+++ routes/parser.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
-
 const parserRouter = require('express').Router();
 
 const parserConfig = fs.readFileSync('./mock-data/parser-config.json', 'utf-8');
-
+const parserGroups = fs.readFileSync('./mock-data/parser-group.json', 'utf-8');
 
 parserRouter.get('/config', function(req, res, next) {
   res.status(200).send(parserConfig);
+});
+
+parserRouter.get('/group', function(req, res, next) {
+  res.status(200).send(parserGroups);
 });
 
 module.exports = parserRouter;
