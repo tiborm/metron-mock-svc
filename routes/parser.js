46a3d4bb5e2c23a318b6af6f0c4820533e79f9ed
tiborm <tibor.meller@gmail.com>
2018-11-10 14:56:45 +0100

    adding config endpoints

--- routes/parser.js
+++ routes/parser.js
@@ -7,6 +7,32 @@ const parserGroups = JSON.parse(fs.readFileSync('./mock-data/parser-group.json',
 parserRouter.get('/config', function(req, res, next) {
   res.status(200).send(JSON.stringify(parserConfig));
 });
+// curl "http://localhost:3000/api/v1/sensor/parser/config/websphere"
+parserRouter.get('/config/:sensorName', function(req, res, next) {
+  const item = parserConfig[req.params.sensorName];
+  item ? res.status(200).send(JSON.stringify(item)) : res.status(404).send();
+});
+// curl --header "Content-Type: application/json" --request POST --data '{"name":"ANiceGroup","description":"Just a nice group of parsers. Edited version #1"}' http://localhost:3000/api/v1/sensor/parser/config/websphere
+parserRouter.post('/config/:sensorName', function(req, res, next) {
+  parserConfig[req.params.sensorName] = req.body;
+  
+  res.status(200).send(
+    JSON.stringify(parserConfig[req.params.sensorName])
+  );
+});
+// curl --request DELETE http://localhost:3000/api/v1/sensor/parser/config/websphere
+parserRouter.delete('/config/:sensorName', function(req, res, next) {
+  if (parserConfig[req.params.sensorName]) {
+    delete parserConfig[req.params.sensorName];
+    res.status(200).send(
+      JSON.stringify({ name: req.params.sensorName, operation: 'delete' })
+    );
+  } else {
+    res.status(404).send(
+      JSON.stringify({ name: req.params.sensorName, operation: 'delete' })
+    );
+  }
+});
 
 // curl "http://localhost:3000/api/v1/sensor/parser/group"
 parserRouter.get('/group', function(req, res, next) {
@@ -14,11 +40,8 @@ parserRouter.get('/group', function(req, res, next) {
 });
 // curl "http://localhost:3000/api/v1/sensor/parser/group/ANiceGroup"
 parserRouter.get('/group/:groupId', function(req, res, next) {
-  res.status(200).send(
-    JSON.stringify(
-      parserGroups.find(group => group.name === req.params.groupId)
-    )
-  );
+  const item = parserGroups.find(group => group.name === req.params.groupId);
+  item ? res.status(200).send(JSON.stringify(item)) : res.status(404).send();
 });
 // curl --header "Content-Type: application/json" --request POST --data '{"name":"ANiceGroup","description":"Just a nice group of parsers. Edited version #1"}' http://localhost:3000/api/v1/sensor/parser/group/ANiceGroup
 parserRouter.post('/group/:groupId', function(req, res, next) {
@@ -40,13 +63,17 @@ parserRouter.post('/group/:groupId', function(req, res, next) {
 });
 // curl --request DELETE "http://localhost:3000/api/v1/sensor/parser/group/ANiceGroup"
 parserRouter.delete('/group/:groupId', function(req, res, next) {
-  parserGroups.splice(
-    parserGroups.indexOf(parserGroups.find(group => group.name === req.params.groupId))
-    , 1)
-    
-  res.status(200).send(
-    JSON.stringify({ name: req.params.groupId, operation: 'delete' })
-  );
+  const item = parserGroups.find(group => group.name === req.params.groupId);
+
+  if (item) {
+    parserGroups.splice(parserGroups.indexOf(item), 1);
+
+    res.status(200).send(
+      JSON.stringify({ name: req.params.groupId, operation: 'delete' })
+    );
+  } else {
+    res.status(404).send();
+  }  
 });
 
 module.exports = parserRouter;
