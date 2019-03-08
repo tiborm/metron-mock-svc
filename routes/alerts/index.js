8f9ae654507be94f6dfdce193a966dfc6009c67b
Tibor Meller <tibor.meller@gmail.com>
2019-03-08 12:12:35 +0100

    adding column/metadata response

--- routes/alerts/index.js
+++ routes/alerts/index.js
@@ -4,4 +4,8 @@ alertsUiRouter.post('/search', function (req, res) {
   res.sendFile(process.cwd() + '/mock-data/alerts/search.json');
 });
 
+alertsUiRouter.post('/column/metadata', function (req, res) {
+  res.sendFile(process.cwd() + '/mock-data/alerts/column-metadata.json');
+});
+
 module.exports = alertsUiRouter;
