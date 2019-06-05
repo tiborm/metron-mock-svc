110c3dadd65a96c9dd12867c86e265068984f725
Tamas Fodor <ftamas.mail@gmail.com>
2019-06-05 14:54:26 +0200

    enrichment-entries: filter by type (#3)

--- routes/enrichments/index.js
+++ routes/enrichments/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const data = require('../../mock-data/enrichments/entries.json');
 
 router.get('/enrichments/:id/entries', function (req, res) {
-  res.status(200).send(data);
+  const type = req.params.id;
+  res.status(200).send(data.filter(entry => entry.type === type));
 });
 
 module.exports = router;
