73138745a0bcf9866df6a5deff61d9b7a5e4330a
ruffle1986 <ftamas.mail@gmail.com>
2019-06-07 14:13:37 +0200

    add get details route

--- routes/enrichments/index.js
+++ routes/enrichments/index.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 const data = require('../../mock-data/enrichments/entries.json');
 
-router.get('/enrichments/:id/entries', function (req, res) {
-  const type = req.params.id;
+router.get('/enrichments/:id', function (req, res) {
+  const id = req.params.id;
+  const entry = data.find(entry => entry.uuid === id);
+  if (entry) {
+    res.status(200).send(entry);
+  } else {
+    res.status(404).send();
+  }
+
+});
+
+router.get('/enrichments/:type/entries', function (req, res) {
+  const type = req.params.type;
   res.status(200).send(data.filter(entry => entry.type === type));
 });
 
